7ae7f3a21baf39d48d29299b8161f07830c676d6
Catherine <catherine.mathenge@strathmore.edu>
2023-06-26 18:10:24 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+  
+    // Add event listener to input element
+    input.addEventListener('click', function() {
+    });
+  }
+  
